15825efa217677c758405fa55e37058d06e016b5
Elanganathapillai Sivakanthan <siva@globalcitymedia.com>
2020-02-26 14:41:11 +0000

    template and static files are added

--- django_apps/urls.py
+++ django_apps/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url
+from django.contrib import admin
+from todoapp.views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name="TodoList"),
 ]
